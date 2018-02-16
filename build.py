20869811b5c382d175b845dd1fad747a41728c6f
Ingo Fruend <mail@ingofruend.net>
2018-02-16 18:07:50 -0500

    initial commit

---
+++ build.py
@@ -0,0 +1,20 @@
+from pybuilder.core import use_plugin, init
+from pybuilder.vcs import count_travis
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+
+name = "esdt"
+default_task = "publish"
+version = count_travis()
+
+
+@init
+def set_properties(project):
+    project.depends_on('numpy')
+    project.depends_on('scipy')
